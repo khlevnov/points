e375ee4172aa503c3b576a9335df00cc21b8ddad
Oleg Khlevnov <oleg@khlevnov.ru>
2017-03-26 05:40:01 +0300

    В package.json добавлены команды генерации случайных точек

--- utils/random/index.js
+++ utils/random/index.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const settings = require('./settings');
 const Random = require('./random');
 
+process.argv.slice(2).forEach(value => {
+    const matches = value.match(/--count=(\w+)/);
+    if (matches) {
+        settings.pointsCount = matches[1];
+    }
+});
+
 const generator = new Random(settings);
 const points = {
     points: generator.generatePoints(),
